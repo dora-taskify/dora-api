847c1ddcbe166bb33d8e4c08647b195c5f20ee01
Aldito Sitompul <98069693+Aldito8@users.noreply.github.com>
2025-09-09 11:10:34 +0700

    [FEAT-BE/8] (#11)
    * [FEAT-BE/8]: logout user

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -1,6 +1,6 @@
 import express from "express"
 import { Request, Response } from "express"
-import { loginAuth, registerAuth } from "@/controllers/auth"
+import { loginAuth, registerAuth, logoutAuth } from "@/controllers/auth"
 import { authenticate } from "@/middlewares/authentication"
 import { errorHandler } from "@/middlewares/errorHandler"
 
@@ -8,6 +8,7 @@ const route = express.Router()
 
 route.post('/register', registerAuth, errorHandler)
 route.post('/login', loginAuth, errorHandler)
+route.post('/logout', logoutAuth, errorHandler)
 route.get('/user', authenticate, (req: Request, res: Response) => {
     const user = (req as any).user;
     res.json(user)
